9de48eab719d9979c2ded323a0f872aa9b099554
Nav Saini <navdeep@mailbox.org>
2022-01-02 17:13:22 +0530

    setup lazygit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,16 @@ vim.opt.termguicolors = true
 vim.o.background = 'dark'
 vim.g.mapleader = ' '
 
+-- setup lazygit
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = 'lazygit',
+  direction = 'float',
+  hidden = true })
+
+function _lazygit_toggle ()
+  lazygit:toggle()
+end
+
 return require('packer').startup(function(use)
 
   use {'akinsho/bufferline.nvim', requires='kyazdani42/nvim-web-devicons'}
@@ -27,6 +37,9 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = {{ 'nvim-lua/plenary.nvim' }}
   }
+
+  use 'akinsho/toggleterm.nvim'
+
 
   use {
     'nvim-treesitter/nvim-treesitter'
@@ -64,45 +77,51 @@ return require('packer').startup(function(use)
       'signify',
       'telescope',
       'treesitter'
-  },
-  langs = {
-    'c',
-    'rust',
-    'html',
-    'golang',
-    'css',
-    'js',
-    'markdown',
-    'python',
-    'typescript',
-    'ocaml',
-    'jsx',
-    'json',
-    'ruby'
+    },
+    langs = {
+      'c',
+      'rust',
+      'html',
+      'golang',
+      'css',
+      'js',
+      'markdown',
+      'python',
+      'typescript',
+      'ocaml',
+      'jsx',
+      'json',
+      'ruby'
+    }
+
+  }))
+
+  require('bufferline').setup{}
+
+  require('pears').setup()
+
+  require'nvim-treesitter.configs'.setup {
+    ensure_installed = 'maintained',
+    sync_install = false,
+    ignore_install = {},
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false
+    }
   }
 
-}))
+  -- vimpeccable keybindings
+  local vimp = require('vimp')
 
-require('bufferline').setup{}
+  vimp.nnoremap('<leader>rc', ':e ~/.config/nvim/init.lua<cr>')
+  vimp.nnoremap('<leader>n', ':NvimTreeToggle<cr>')
+  vimp.nnoremap('<leader>s', ':w<cr>')
+  vimp.nnoremap('<leader>e', ':q!<cr>')
+  vimp.nnoremap('<leader>ff', ':Telescope find_files<CR>')
 
-require('pears').setup()
-
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'maintained',
-  sync_install = false,
-  ignore_install = {},
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  }
-}
-
--- vimpeccable keybindings
-local vimp = require('vimp')
-
-vimp.nnoremap('<leader>ev', ':e ~/.config/nvim/init.lua<cr>')
-vimp.nnoremap('<leader>n', ':NvimTreeToggle<cr>')
-vimp.nnoremap('<leader>s', ':w<cr>')
-vimp.nnoremap('<leader>ff', ':Telescope find_files<CR>')
+  vim.api.nvim_set_keymap('n', '<leader>g', "<cmd>lua _lazygit_toggle()<CR>",
+  {noremap = true, silent = true })
 
 end)
+
+
