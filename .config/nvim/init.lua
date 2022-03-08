578d35d377f341f900e199db7559212c0d00f5e2
Nav Saini <navdeep@mailbox.org>
2022-03-08 20:21:43 +0530

    nvim: enable tabout

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -128,7 +128,67 @@ cmp.setup.cmdline(
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require("lspconfig")["tsserver"].setup {
-  capabilities = capabilities
+  init_options = require("nvim-lsp-ts-utils").init_options,
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    local ts_utils = require("nvim-lsp-ts-utils")
+
+    -- defaults
+    ts_utils.setup(
+      {
+        debug = false,
+        disable_commands = false,
+        enable_import_on_completion = false,
+        -- import all
+        import_all_timeout = 5000, -- ms
+        -- lower numbers = higher priority
+        import_all_priorities = {
+          same_file = 1, -- add to existing import statement
+          local_files = 2, -- git files or files with relative path markers
+          buffer_content = 3, -- loaded buffer content
+          buffers = 4 -- loaded buffer names
+        },
+        import_all_scan_buffers = 100,
+        import_all_select_source = false,
+        -- if false will avoid organizing imports
+        always_organize_imports = true,
+        -- filter diagnostics
+        filter_out_diagnostics_by_severity = {},
+        filter_out_diagnostics_by_code = {},
+        -- inlay hints
+        auto_inlay_hints = true,
+        inlay_hints_highlight = "Comment",
+        inlay_hints_priority = 200, -- priority of the hint extmarks
+        inlay_hints_throttle = 150, -- throttle the inlay hint request
+        inlay_hints_format = {
+          -- format options for individual hint kind
+          Type = {},
+          Parameter = {},
+          Enum = {}
+          -- Example format customization for `Type` kind:
+          -- Type = {
+          --     highlight = "Comment",
+          --     text = function(text)
+          --         return "->" .. text:sub(2)
+          --     end,
+          -- },
+        },
+        -- update imports on file move
+        update_imports_on_move = false,
+        require_confirmation_on_move = false,
+        watch_dir = nil
+      }
+    )
+
+    -- required to fix code action ranges and filter diagnostics
+    ts_utils.setup_client(client)
+
+    -- no default maps, so you may want to define some here
+    local opts = {silent = true}
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
+  end
 }
 
 return require("packer").startup(
@@ -139,6 +199,14 @@ return require("packer").startup(
 
     use {"rcarriga/vim-ultest", requires = {"vim-test/vim-test"}, run = ":UpdateRemotePlugins"}
 
+    use {
+      "abecodes/tabout.nvim",
+      config = function()
+        require("tabout").setup {}
+      end,
+      wants = {"nvim-treesitter"},
+      after = {"nvim-cmp"}
+    }
     use "p00f/nvim-ts-rainbow"
     use {
       "datwaft/bubbly.nvim",
@@ -177,6 +245,8 @@ return require("packer").startup(
     use "hrsh7th/cmp-path"
     use "hrsh7th/cmp-cmdline"
     use "hrsh7th/nvim-cmp"
+
+    use "jose-elias-alvarez/nvim-lsp-ts-utils"
 
     use "nathom/filetype.nvim"
     use "saadparwaiz1/cmp_luasnip"
