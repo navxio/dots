d5e0f19c8f7634b489a02ab4ae32542871bc8c68
Nav Saini <navdeep@mailbox.org>
2022-04-04 23:50:24 +0530

    nvim: snippet expansion coexists with tabout.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -79,6 +79,16 @@ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
 
+local function prequire(...)
+	local status, lib = pcall(require, ...)
+	if status then
+		return lib
+	end
+	return nil
+end
+
+local luasnip = prequire("luasnip")
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -95,6 +105,24 @@ cmp.setup({
 			c = cmp.mapping.close(),
 		}),
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+			else
+				fallback()
+			end
+		end,
+		["<S-Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+			else
+				fallback()
+			end
+		end,
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
@@ -412,15 +440,6 @@ return require("packer").startup(function(use)
 		},
 	})
 
-	local function prequire(...)
-		local status, lib = pcall(require, ...)
-		if status then
-			return lib
-		end
-		return nil
-	end
-
-	local luasnip = prequire("luasnip")
 	local cmp = prequire("cmp")
 
 	local t = function(str)
